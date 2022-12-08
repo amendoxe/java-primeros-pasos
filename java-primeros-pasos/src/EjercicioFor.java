b9624aca17d212ef0072edeebd5f14e806fe850d
amendoxe <arturo_mendoza@outlook.com>
2022-12-07 22:48:46 -0600

    Add EjercicioFor

---
+++ java-primeros-pasos/src/EjercicioFor.java
@@ -0,0 +1,10 @@
+public class EjercicioFor {
+	public static void main(String[] args) {
+//Imprimir todos los múltiplos de 3 entre 1 y 100
+		for (int i = 0; i <= 100; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
