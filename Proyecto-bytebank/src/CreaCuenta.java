bac2eb325dd4c7450c3431db3f72ce784ea53ef5
amendoxe <arturo_mendoza@outlook.com>
2022-12-08 20:52:45 -0600

    Add some code

--- Proyecto-bytebank/src/CreaCuenta.java
+++ Proyecto-bytebank/src/CreaCuenta.java
@@ -3,10 +3,12 @@ public class CreaCuenta {
 	public static void main(String[] args) {
 
 		Cuenta primeraCuenta = new Cuenta();
-		primeraCuenta.saldo = 200;
-		primeraCuenta.numero = 320948;
-		primeraCuenta.sucursal = 324;
-		primeraCuenta.nombre = "Fulanite something something";
 
+		primeraCuenta.saldo = 1000;
+//		primeraCuenta.numero = 320948;
+//		primeraCuenta.sucursal = 324;
+//		primeraCuenta.nombre = "Fulanite something something";
+
+		System.out.println(primeraCuenta.saldo);
 	}
 }
